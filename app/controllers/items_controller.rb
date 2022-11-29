6aa5cb313e86bdc1cf7c16475bcf16f934a6245d
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-11-29 14:33:06 -0700

    commit tree 043edbbeccbbd4e9ec6e4c1f47174b58c61c9efc

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :display_error
+  rescue_from ActiveRecord::RecordInvalid, with: :display_error
+
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else 
+      items = Item.all
+    end
+      render json: items, include: :user
   end
 
+  def create
+    item = Item.create!(item_params)
+    if params[:user_id]
+      item.update(user_id: params[:user_id])
+    end
+    render json: item, status: :created
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  private
+
+  def display_error (error)
+    render json: error, status: 404
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :display_error
+
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
+  end
+
+  private
+
+  def display_error (error)
+    render json: error, status: 404
   end
 
 end
